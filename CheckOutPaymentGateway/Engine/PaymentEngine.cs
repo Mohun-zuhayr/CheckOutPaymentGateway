619ef29df5103bd9b3026023d75952cac74ab330
Mohun, Zuhayr <Zuhayr.Mohun@ceridian.com>
2020-05-11 12:57:34 +0400

    Updates

--- CheckOutPaymentGateway/Engine/PaymentEngine.cs
+++ CheckOutPaymentGateway/Engine/PaymentEngine.cs
@@ -4,20 +4,22 @@ using ExternalBankWrapper.Expo;
 using ExternalBankWrapper.Models;
 using NLog;
 using System;
+using Serilog;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using States = CheckOutPaymentGatewayWrapper.Model.States;
+using CheckOutPaymentGateway.EncryptionEngine;
 
 namespace CheckOutPaymentGateway.Engine
 {
-    public class PaymentEngine: IPaymentEngine, IDisposable
+    public class PaymentEngine : IPaymentEngine, IDisposable
     {
         private PaymentGatewayValidator validator = null;
+        private const string secretKey = "ssshhhhhhhhhhh!!!!";
         private static Logger logger = LogManager.GetCurrentClassLogger();
-
         #region constructor
         public PaymentEngine()
         {
@@ -28,16 +30,29 @@ namespace CheckOutPaymentGateway.Engine
         #region Interface Implementation
         public async Task<ResponseAPIModel> ProcessPayment(ProcessPaymentAPIModel paymentModel)
         {
+            //Serilog implementation
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.Seq("http://localhost:5341")
+            .CreateLogger();
+
             logger.Info("Starting Process Payment...");
+            Log.Information("Starting Process Payment...");
             logger.Info("Validating process payment model");
+            Log.Information("Validating process payment model");
+
             if (paymentModel != null && validator.ValidatePaymentModel(paymentModel, logger))
             {
                 logger.Info("Valid payment model");
+                Log.Information("Valid payment model");
+
                 ProcessPaymentModel processModel = BuildPaymentModel(paymentModel);
 
                 //contact and expose data to external bank api
                 ExternalBankExpo contactExternalBank = new ExternalBankExpo();
                 logger.Info("Contacting external bank endpoints");
+                Log.Information("Contacting external bank endpoints");
+
                 try
                 {
                     var response = contactExternalBank.ProcessPayment(processModel);
@@ -45,11 +60,15 @@ namespace CheckOutPaymentGateway.Engine
                     if (response != null && response.ResponseStatus == States.Success.ToString())
                     {
                         logger.Info("Success in contacting external bank endpoints");
+                        Log.Information("Success in contacting external bank endpoints");
+
 
                         //build transaction model and save data to db
                         Transaction transaction = BuildTransactionModel(processModel, response);
 
                         logger.Info("Attempting to save transaction");
+                        Log.Information("Attempting to save transaction");
+
                         try
                         {
                             using (CheckOutPaymentGatewayEntities db = new CheckOutPaymentGatewayEntities())
@@ -68,22 +87,29 @@ namespace CheckOutPaymentGateway.Engine
                         catch (Exception ee)
                         {
                             logger.Error("Failed to contact perform transaction - " + ee.ToString());
+                            Log.Information("Failed to contact perform transaction - " + ee.ToString());
+
                         }
                     }
                     else
                     {
                         logger.Error("Failed to contact external bank endpoints");
+                        Log.Information("Failed to contact external bank endpoints");
+
                         return new ResponseAPIModel(States.Error, "Cannot contact and process payment to external bank");
                     }
                 }
                 catch (Exception e)
                 {
                     logger.Error("Failed to contact external bank endpoints - " + e.ToString());
+                    Log.Information("Failed to contact external bank endpoints - " + e.ToString());
+
                     return new ResponseAPIModel(States.Error, "Cannot contact and process payment to external bank");
                 }
-                
+
             }
             logger.Error("Invalid payment model");
+            Log.CloseAndFlush();
             return new ResponseAPIModel(States.Error, "Invalid payment api model");
         }
 
@@ -99,7 +125,7 @@ namespace CheckOutPaymentGateway.Engine
             {
                 Amount = x.Amount,
                 CardExpiryDate = x.CardExpiryDate,
-                MaskedCardNumber = PrepareMask(x.CardNumber),
+                MaskedCardNumber = PrepareMask(EncryDecryMechanism.DecryptData(x.CardNumber, secretKey)),
                 CardCurrency = x.CardCurrency,
                 TransactionStatus = x.TransactionStatus,
                 TransactionDateTime = x.TransactionDateTime
@@ -109,19 +135,18 @@ namespace CheckOutPaymentGateway.Engine
         public void Dispose()
         {
             validator = null;
-        } 
+        }
         #endregion
 
         #region Private Methods
         private static Transaction BuildTransactionModel(ProcessPaymentModel processModel, ResponseModel response)
         {
             logger.Info("Build transaction models");
-
             return new Transaction
             {
                 TransactionId = response.TransactionID,
                 MerchantId = processModel.MerchantId,
-                CardNumber = processModel.CardNumber,
+                CardNumber = EncryDecryMechanism.EncryptData(processModel.CardNumber, secretKey),
                 CardExpiryDate = processModel.ExpiryDate,
                 CardCurrency = processModel.Currency,
                 Amount = processModel.Amount,
@@ -137,7 +162,7 @@ namespace CheckOutPaymentGateway.Engine
             {
                 MerchantId = paymentModel.MerchantId,
                 Amount = paymentModel.Amount,
-                CardNumber = paymentModel.CardNumber,
+                CardNumber = EncryDecryMechanism.EncryptData(paymentModel.CardNumber, secretKey),
                 CVV = paymentModel.CVV,
                 Currency = paymentModel.Currency,
                 ExpiryDate = paymentModel.ExpiryDate
